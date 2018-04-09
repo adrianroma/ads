90febc92eacc2fa8106472d92cff20cace2afaa3
Adrian Romero Vargas <ak360s@gmail.com>
2018-04-09 17:36:07 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,140 @@
+{
+    
+	      "mantenimiento":{
+				
+				          "preventivo":{
+				                  "NAME":["mantenimiento:90"],
+				              
+				                  "DURACION":{
+								          "DESDE":["01/03/18:90"],
+                                          "HASTA":["01/06/18:90"]								  
+								  
+								  }, 
+								  "DISPONIBILIDAD":{
+								           "ESTA":["disponible:90"], 
+								           "TOTAL":["2:90"]
+								  },
+				                  "REQUERIMIENTOS":{ 
+								         "DIRECCION":["atzcapozalco:90","gustavo a madero:70","alvaro obregon:50"],
+										 "TRABAJO ANTERIOR":["mantenimiento:90","reparacion:50","servicio:70"],
+										 "PRESENTACION":["trabajador:90"],
+								         "HABILIDADES":["multimetro:90","soldadora:50","computadora:80"],
+										 "ACTITUDES":["puntual:90","pro activo:60"],
+										 "SEXO":["hombre:40","mujer:60","otro:80"],
+										 "EDAD":["18:90","45:20"],
+										 "TEMPERAMENTO":["extrovertido:90"],
+										 "RESPONSABILIDADES":["electricidad:80","plomeria:90","refrigeracion:30"],
+								         "EXPERIENCIA":{
+										                 "AÑOS":["0:0","1:90"],
+                                                         "MESES":["0:0","12:90"],
+                                                         "HORAS":["0:0","1000:90"]  														 
+										               },
+								         "LENGUAJES":["español:90","ingles:50"],
+										 
+								         "EDUCACION":["prepa:50","bachillerato:50","tecnico:90"]
+								   
+				                                  },
+                                   "OFERTA":{
+								          "VACANTE":["Jefe de Mantenimiento de Tienda:90"],
+								          "SALARIO":["5,000:90","10,000:50"],
+										  "RESPONSABLIDIADES":["instalaciones de gas:90","instalaciones de toda la planta:50"],
+                                          "PRESTACIONES":["imss:90","bono por productividad:80","caja de ahorro:90","prestaciones superiores a la Ley:50"],
+										  "CRECIMIENTO":["en 6 meses:50"],
+                                          "CAPACITACION":["continua:90","mantenimiento preventivo:60"],										  
+                                          "HORARIO":["8 00 am a 1:00 pm:90","6 00 pm a 11 00 pm:50"]										  
+						
+								   
+								 },
+                                  "OFERTADOR":{
+								          "NOMBRE":["WALLMART DE MEXICO:90"],
+										  "TIPO":["Privado:90"],
+                                          "DIRECCION":["Av. Universidad 936-A, Sta Cruz Atoyac, 03310 Ciudad de México:80"],
+										  "PRESENTACION":["Buscamos al mejor talento de mexico:90"],
+                                          "ACERCA":["Somos una empresa Mexicana en busca de nuevo talento:90"]  
+ 										  
+								  },
+                                 "PRE ENTREVISTA":{
+								         "MOBIL":["535353535:90","34535359888:90"],
+										 "ENTREVISTADOR":["Alex:90","Maria:50"],
+								         "TELEFONO":["33435454545:90"],
+										 "FECHA":["01/04/18:90","02/04/18:50"],
+                                         "HORARIOS":["10 30 am:90","4 30 am:90"],										 
+								         "DIRECCION":["Real de Mayorazgo 23:90"],
+										 "REFERENCIA":["Esquina de Radio Formula:90"],
+										 "REQUSITOS":["IFE:90","Curriculum Impreso:50"],
+										 "INSTRUCCIONES":["En el edificio A, preguntar por la puerta C ahi te recibimos:90"],
+										 "MAPA":[""]
+								 }	
+                           }								 
+	
+
+                    },
+					
+	            "alamacenista":{
+				                  "NAME":["almacenista:90"],
+				              
+				                  "DURACION":{
+								          "DESDE":["01/03/18:90"],
+                                          "HASTA":["01/06/18:90"]								  
+								  
+								  }, 
+								  "DISPONIBILIDAD":{
+								           "ESTA":["disponible:90"], 
+								           "TOTAL":["2:90"]
+								  },
+				                  "REQUERIMIENTOS":{ 
+								         "DIRECCION":["miguel hidalgo:90","cuahutemoc:70","coyoacan:50"],
+										 "TRABAJO ANTERIOR":["almacen:90","bodega:50","inventario:70"],
+										 "PRESENTACION":["trabajador:90"],
+								         "HABILIDADES":["excel:90","windows:50","csv","computadora:80"],
+										 "ACTITUDES":["puntual:90","preciso:60"],
+										 "SEXO":["hombre:40","mujer:60","otro:80"],
+										 "EDAD":["18:90","45:20"],
+										 "TEMPERAMENTO":["introvertido:90"],
+										 "RESPONSABILIDADES":["inventario:80","cuidar:90","reportar:80"],
+								         "EXPERIENCIA":{
+										                 "AÑOS":["0:0","1:90"],
+                                                         "MESES":["0:0","12:90"],
+                                                         "HORAS":["0:0","1000:90"]  														 
+										               },
+								         "LENGUAJES":["español:90","ingles:50"],
+										 
+								         "EDUCACION":["prepa:50","bachillerato:50"]
+								   
+				                                  },
+                                   "OFERTA":{
+								          "VACANTE":["Almacenista:90"],
+								          "SALARIO":["2,000:90","3,000:50"],
+										  "RESPONSABLIDIADES":["Cuidar el inventario:90","reportar a supervisores:50"],
+                                          "PRESTACIONES":["imss:90","bono por puntualidad:80","caja de ahorro:90","prestaciones superiores a la Ley:50"],
+										  "CRECIMIENTO":["en 6 meses:50"],
+                                          "CAPACITACION":["idioma ingles:90"],										  
+                                          "HORARIO":["8 00 am a 1:00 pm:90","6 00 pm a 11 00 pm:50"]										  
+						
+								   
+								 },
+                                  "OFERTADOR":{
+								          "NOMBRE":["WALLMART DE MEXICO:90"],
+										  "TIPO":["Privado:90"],
+                                          "DIRECCION":["Av. Universidad 936-A, Sta Cruz Atoyac, 03310 Ciudad de México:80"],
+										  "PRESENTACION":["Buscamos al mejor talento de mexico:90"],
+                                          "ACERCA":["Somos una empresa Mexicana en busca de nuevo talento:90"]  
+ 										  
+								  },
+                                 "PRE ENTREVISTA":{
+								         "MOBIL":["535353535:90","34535359888:90"],
+										 "ENTREVISTADOR":["Alex:90","Maria:50"],
+								         "TELEFONO":["33435454545:90"],
+										 "FECHA":["01/04/18:90","02/04/18:50"],
+                                         "HORARIOS":["10 30 am:90","4 30 am:90"],										 
+								         "DIRECCION":["Real de Mayorazgo 23:90"],
+										 "REFERENCIA":["Esquina de Radio Formula:90"],
+										 "REQUSITOS":["IFE:90","Curriculum Impreso:50"],
+										 "INSTRUCCIONES":["En el edificio A, preguntar por la puerta C ahi te recibimos:90"],
+										 "MAPA":[""]
+								 }								  
+	
+
+                    }					
+
+}
